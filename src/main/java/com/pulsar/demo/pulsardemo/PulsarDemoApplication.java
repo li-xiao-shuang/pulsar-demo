e0ecc2d4038dcdca7f6b87dfcb5fe2bca2523575
lixiaoshuang <lixiaoshuang@didiglobal>
2021-11-11 20:29:06 +0800

    初始化

---
+++ src/main/java/com/pulsar/demo/pulsardemo/PulsarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pulsar.demo.pulsardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsarDemoApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PulsarDemoApplication.class, args);
+    }
+    
+}
