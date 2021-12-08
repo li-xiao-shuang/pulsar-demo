163b5289d1cb9002ee8e6cbeeeb9d91bfda053b5
lixiaoshuang <644968328@qq.com>
2021-12-08 20:56:30 +0800

    添加demo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # pulsar-demo
 pulsar项目实验
+
+
+# Producer
+- 发送模式
+- 访问模式
+
+# Consumer
--- src/main/java/com/pulsar/demo/pulsardemo/ProducerDemo.java
+++ src/main/java/com/pulsar/demo/pulsardemo/ProducerDemo.java
@@ -13,42 +13,64 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.pulsar.demo.pulsardemo;
 
 import lombok.Data;
 import org.apache.pulsar.client.api.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author lixiaoshuang
  */
 @Data
 public class ProducerDemo {
-
-
-    public static void main(String[] args) throws PulsarClientException {
-        // 创建pulsar客户端
-        PulsarClient client = PulsarClient.builder()
-                .serviceUrl("pulsar://101.42.108.126:6650")
-                .build();
-        // 创建生产者
-        Producer<byte[]> producer = client.newProducer().topic("test-topic-1").create();
-        producer.send("发送一条字符串消息".getBytes());
-
+    
+    private static PulsarClient PULSAR_CLIENT = null;
+    
+    static {
+        try {
+            // 创建pulsar客户端
+            PULSAR_CLIENT = PulsarClient.builder().serviceUrl("pulsar://101.42.108.126:6650").build();
+        } catch (PulsarClientException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public static void main(String[] args) throws PulsarClientException, ExecutionException, InterruptedException {
+        
+        // 发送方式测试
+        sendTest();
+        
         // 创建消费者
-        Consumer<byte[]> consumer = client.newConsumer()
-                .topic("test-topic-1")
-                .subscriptionName("test-subscription-1")
-                .subscribe();
-
+        Consumer<byte[]> consumer = PULSAR_CLIENT.newConsumer().topic("test-topic-1")
+                .subscriptionName("test-subscription-1").subscribe();
+        
         //获取消息内容
         Message<byte[]> message = consumer.receive();
         System.out.println("接受消息内容: " + new String(message.getData()));
         // 确认消费成功，以便pulsar删除消费成功的消息
         consumer.acknowledge(message);
-
+        
         //关闭客户端
-        producer.close();
+        //        producer.close();
         consumer.close();
-        client.close();
+        PULSAR_CLIENT.close();
+    }
+    
+    private static void sendTest() throws PulsarClientException, InterruptedException, ExecutionException {
+        // 创建生产者
+        Producer<byte[]> producer = PULSAR_CLIENT.newProducer().topic("test-topic-1").create();
+        // 同步发送消息
+        MessageId messageId = producer.send("同步发送的消息".getBytes(StandardCharsets.UTF_8));
+        System.out.println("同步发送，消息id: " + messageId);
+        
+        CompletableFuture<MessageId> messageIdCompletableFuture = producer.sendAsync(
+                "异步发送的消息".getBytes(StandardCharsets.UTF_8));
+        System.out.println("异步发送，消息id：" + messageIdCompletableFuture.get());
     }
 }
